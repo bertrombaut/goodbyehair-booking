99f73e2fc0e4579f24deab4fa94cb34f15bb5a6a
bertrombaut <bertrombaut14081966@gmail.com>
2026-03-31 13:40:18 +0200

    Update goodbyehair-booking.php

--- goodbyehair-booking.php
+++ goodbyehair-booking.php
@@ -45,7 +45,7 @@ class GBH_Booking {
 
         ob_start();
 
-        echo '<h2>Kies je behandeling TeST op disndag 31 maart om 13:35 uur</h2>';
+        echo '<h2>Kies je behandeling TeST op disndag 31 maart om 13:40 uur</h2>';
 
         foreach ($treatments as $category => $items) {
 
